42674c6af69397e2eeb267a0f644276610243abd
Jonathan <jonathanjacka@gmail.com>
2020-08-16 21:47:43 -0500

    Updated lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Color(0xff455A64),
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text('Roll the dice!'),
+          backgroundColor: Color(0xff607d8b),
         ),
         body: DicePage(),
       ),
@@ -18,6 +18,21 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          //flex: 1,
+          child: Image.asset(
+            'images/dice1.png',
+          ),
+        ),
+        Expanded(
+          //flex: 1,
+          child: Image.asset(
+            'images/dice2.png',
+          ),
+        )
+      ],
+    );
   }
 }
